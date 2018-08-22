eae36e354ce720c64e001fe2cc253f46e072201a
44Schwarz <sirmitya@gmail.com>
2018-08-22 17:46:04 +0300

    Create models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Dictionary, Word, Usage, Language
+
 
 # Register your models here.
+admin.site.register(Dictionary)
+admin.site.register(Word)
+admin.site.register(Usage)
+admin.site.register(Language)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Dictionary(models.Model):
+    language_from = models.ForeignKey(Language, related_name='language_from', on_delete=models.PROTECT)
+    language_to = models.ForeignKey(Language, related_name='language_to', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{} - {}'.format(self.language_from, self.language_to)
+
+    class Meta:
+        verbose_name_plural = 'dictionaries'
+
+
+class Word(models.Model):
+    name = models.CharField(max_length=50)
+    definition = models.TextField()
+    dictionary = models.ForeignKey(Dictionary, blank=True, null=True, on_delete=models.CASCADE)
+    translation = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Usage(models.Model):
+    text = models.TextField()
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.text[:40]) + ' ...' * int(len(self.text) > 40)
